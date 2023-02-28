b9c3ee9168a6aadbb0826580da415d7aaece6592
Louis B <barretolouis5@gmail.com>
2023-02-27 22:01:49 -0600

    added script.js/added intitialCards array with objs

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "L'Eau",
+    link: "https://images.unsplash.com/photo-1583325033548-1eeacdb0b16e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=856&q=80",
+  },
+  {
+    name: "Palace of Versailles painting",
+    link: "https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+  },
+  {
+    name: "Layers of wheatpasted posters on a wall",
+    link: "https://images.unsplash.com/photo-1583407723467-9b2d22504831?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  {
+    name: "Abandoned bike",
+    link: "https://images.unsplash.com/photo-1585644198527-05519fdeaf98?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  },
+  {
+    name: "Rabbit man",
+    link: "https://images.unsplash.com/photo-1624930266067-dc3aa6a7fd5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+  {
+    name: "mental health matters",
+    link: "https://images.unsplash.com/photo-1634424332312-13da1be082a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+];
